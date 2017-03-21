6ac1e9912efaac1732fdf77613eb3490f39b74c2
Crazy_Cactus <Mathias.Genesis@Gmail.com>
2017-03-21 11:19:12 +0100

    Amelioration du panneau et de la presentation

--- fonction.php
+++ fonction.php
@@ -20,4 +20,33 @@
     $resultat = $db->query('SELECT * FROM users');
     return $resultat;
   }
+
+function ping($link)
+{
+  exec("ping ".$link, $ping);
+  if(!empty($ping[10]))
+  {
+    ?>
+      <div class="panneau">
+        <ul>
+          <li><?php echo('<span class="online">ONLINE</span>'); ?></li>
+          <li><?php echo('SERVEUR : ' . $link); ?></li>
+          <li><?php echo "<br/>"; ?></li>
+          <li><?php echo ("Ping = " . substr($ping[10], strrpos($ping[10], '=') + 1)); ?></li>
+        </ul>
+      </div>
+    <?php
+  }
+  else
+  {
+    ?>
+      <div class="panneau">
+        <ul>
+          <li><?php echo '<span class="offline">OFFLINE</span>'; ?></li>
+          <li><?php echo('SERVEUR : ' . $link); ?></li>
+        </ul>
+      </div>
+    <?php
+  }
+}
 ?>
--- ping.php
+++ ping.php
@@ -2,24 +2,12 @@
 require_once('init.php');
 require_once('header.php');
 $page = 'ping';
+$serveurs = ['google.fr', 'ffring.com', 'tamer.com'];
 
-  exec("ping google.fr", $ping);
-  if(isset($ping[10]))
-  {
-    ?>
-      <p class="panneau">
-        <?php echo('ONLINE : Ping = ' . substr($ping[10], -6)); ?>
-      </p>
-    <?php
-  }
-  else
-  {
-    ?>
-      <p>
-        <?php echo "OFFLINE"; ?>
-      </p>
-    <?php
-  }
+foreach ($serveurs as $serveur)
+{
+  ping($serveur);
+}
 ?>
 <a href="ping.php">PING</a>
 
